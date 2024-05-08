018d4f600c1c275eebb7b986aa5957a55b0bde36
SeokhunEom <tommya98@naver.com>
2024-05-08 11:17:10 +0900

    FIX: 삼성 인터넷 그룹 입력 버그 수정

--- src/components/AddMembers.tsx
+++ src/components/AddMembers.tsx
@@ -3,6 +3,7 @@ import { useRecoilState, useRecoilValue } from 'recoil';
 import { InputTags } from 'react-bootstrap-tagsinput';
 import styled from 'styled-components';
 import { useNavigate } from 'react-router-dom';
+import { Form } from 'react-bootstrap';
 import CenteredOverlayForm from './shared/CenteredOverlayForm';
 import groupMembersState from '../state/groupMembers';
 import groupNameState from '../state/groupName';
@@ -13,20 +14,25 @@ type HandleSubmitType = (event: React.FormEvent<HTMLFormElement>) => void;
 function AddMembers() {
   const [groupMembers, setGroupMembers] = useRecoilState(groupMembersState);
   const groupName = useRecoilValue(groupNameState);
+  const [groupMembersString, setGroupMembersString] = useState('');
   const [validated, setValidated] = useState(false);
   const navigate = useNavigate();
   const title = `${groupName} 그룹에 추가할 멤버들의 이름을 입력해 주세요.`;
+  const isSamsungInternet = window.navigator.userAgent.includes('SAMSUNG');
 
   const handleSubmit: HandleSubmitType = (event) => {
     event.preventDefault();
 
     setValidated(true);
     if (groupMembers.length > 0) navigate(ROUTES.EXPENSE_MAIN);
+    else if (groupMembersString.length > 0) setGroupMembers(groupMembersString.split(','));
   };
 
   return (
     <CenteredOverlayForm title={title} validated={validated} handleSubmit={handleSubmit}>
-      <InputTags placeholder="이름 간에 띄어 쓰기" data-testid="input-member-names" onTags={(value) => setGroupMembers(value.values)} />
+      {isSamsungInternet
+        ? <Form.Control placeholder="이름 간 컴마(,)로 구분" onChange={(event) => setGroupMembersString(event.target.value)} />
+        : <InputTags placeholder="이름 간에 띄어 쓰기" data-testid="input-member-names" onTags={(value) => setGroupMembers(value.values)} />}
       {validated && groupMembers.length === 0 && <StyledErrorMessage>그룹 멤버들의 이름을 입력해 주세요.</StyledErrorMessage>}
     </CenteredOverlayForm>
   );
